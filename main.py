c7ec3146b1dde157ef6c757de72ae90d8154936b
a1ex-w <alexwittman@live.com>
2025-11-06 19:06:53 -0800

    draw player to screen

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-
+from logger import log_state
+from player import Player
 
 
 
 def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        log_state()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
