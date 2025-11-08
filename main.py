7a5ca4a9b80bac65e86d8d460e6ebe71cbe47b2e
a1ex-w <alexwittman@live.com>
2025-11-07 19:38:37 -0800

    added full player movement and groups for updatable and drawable objects

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -19,11 +22,12 @@ def main():
                 return
 
         log_state()
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
